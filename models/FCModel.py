70a87a03852c2a997a86f5f0e38baebf2bca910c
raqoon886 <wieder886@gmail.com>
2022-11-09 11:20:56 +0900

    chg variable name

--- models/FCModel.py
+++ models/FCModel.py
@@ -1,21 +1,18 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from utils import *
 
 
 class FCModel(nn.Module):
-    def __init__(self, training=True):
+    def __init__(self, text_dim, affiliation_dim, year_dim):
         super(FCModel, self).__init__()
-        self.textfc = nn.Linear(1000, 100)
-        # self.authorfc = nn.Linear(???, 100)
-        self.yearfc = nn.Linear(13, 2)
-        self.training = training
+        self.text_fc = nn.Linear(text_dim, 100)
+        self.affiliation_fc = nn.Linear(affiliation_dim, 100)
+        self.year_fc = nn.Linear(year_dim, 2)
 
     def forward(self, data):
-        text_emb, year_emb = text_corpus_and_year_embedding(data)
-        text_emb = self.textfc(text_emb)
-        # author_emb = self.authorfc(author_emb)
-        year_emb = self.yearfc(year_emb)
-        # return torch.cat([text_emb, author_emb, year_emb])
-        return torch.cat([text_emb, year_emb])
+        text, affiliation, year = data
+        text_emb = self.text_fc(text)
+        affiliation_emb = self.affiliation_fc(affiliation)
+        year_emb = self.year_fc(year)
+
+        return torch.cat([text_emb, affiliation_emb, year_emb])
