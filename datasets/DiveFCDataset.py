4643215a9ec95f713f052f98d96827322a35d0d2
hwCh00 <polyseng@yonsei.ac.kr>
2022-11-10 16:39:45 +0000

    error handling

--- datasets/DiveFCDataset.py
+++ datasets/DiveFCDataset.py
@@ -1,3 +1,5 @@
+import torch
+import numpy as np
 import torch.utils.data as data
 from .utils import construct_fc_data
 
@@ -8,13 +10,17 @@ class DiveFCDataset(data.Dataset):
 
         self.batch_size = 128
         
-        paper_ids, texts, years, authors, labels = construct_data(dir_path, affiliation_path, citation_threshold)
-        self.data = torch.FloatTensor([paper_ids, texts, years, authors])
-        self.target = torch.FloatTensor(labels)
+        paper_ids, texts, years, authors, labels = construct_fc_data(dir_path, affiliation_path, citation_threshold)
+
+        self.paper_ids = paper_ids
+        self.data = torch.FloatTensor(np.hstack([texts, years, authors]))
+        # self.data = torch.FloatTensor(np.array([texts, years, authors]))
+        self.target = torch.Tensor(labels)
         
 
     def __getitem__(self, index):
-        return self.data[index], self.target[index]
+        return self.paper_ids[index], self.data[index], self.target[index]
 
     def __len__(self):
-        return len(self.target)
+        return self.data.shape[0]
+    
