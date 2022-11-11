74ab80918af610e36117eed0eccc335749ce9eb6
raqoon886 <wieder886@gmail.com>
2022-11-11 22:18:16 +0900

    add one fc layer

--- models/FCModel.py
+++ models/FCModel.py
@@ -10,9 +10,15 @@ class FCModel(nn.Module):
         self.text_dim = text_dim
         self.affiliation_dim = affiliation_dim
         self.year_dim = year_dim
-        
-        self.text_fc = nn.Linear(text_dim, 100)
-        self.affiliation_fc = nn.Linear(affiliation_dim, 100)
+
+        self.text_fc = nn.ModuleList()
+        self.text_fc.append(nn.Linear(text_dim, 256))
+        self.text_fc.append(nn.Linear(256, 100))
+
+        self.affiliation_fc = nn.ModuleList()
+        self.affiliation_fc.append(nn.Linear(affiliation_dim, 256))
+        self.affiliation_fc.append(nn.Linear(256, 100))
+
         self.year_fc = nn.Linear(year_dim, 2)
 
     def forward(self, data):
@@ -20,9 +26,15 @@ class FCModel(nn.Module):
         text = data[:,:self.text_dim]
         affiliation = data[:,self.text_dim:self.text_dim+self.affiliation_dim]
         year = data[:,-self.year_dim:]
-        
-        text_emb = F.relu(self.text_fc(text))
-        affiliation_emb = F.relu(self.affiliation_fc(affiliation))
-        year_emb = F.relu(self.year_fc(year))
 
-        return torch.cat([text_emb, affiliation_emb, year_emb], dim=1)
+        for i in range(len(self.text_fc)):
+            text = self.text_fc[i](text)
+            text = F.relu(text)
+
+        for i in range(len(self.affiliation_fc)):
+            affiliation = self.affiliation_fc[i](affiliation)
+            affiliation = F.relu(affiliation)
+
+        year = F.relu(self.year_fc(year))
+
+        return torch.cat([text, affiliation, year], dim=1)
